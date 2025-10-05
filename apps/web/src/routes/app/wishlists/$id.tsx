6ed023c21f18127b57d5a816d50a0a39ace76731
Oleksandr Havrylov <47502748+SashkaHavr@users.noreply.github.com>
2025-10-05 20:41:30 +0000

    Update wishlist page

--- apps/web/src/routes/app/wishlists/$id.tsx
+++ apps/web/src/routes/app/wishlists/$id.tsx
@@ -6,6 +6,7 @@ import { Separator } from '~/components/ui/separator';
 
 import { AppDialogTrigger } from '~/components/app-dialog';
 import { UpdateWishlistDialog } from '~/components/app/update-wishlist-button';
+import { PageLayout } from '~/components/page-layout';
 import { useTRPC } from '~/lib/trpc';
 import { wishlistGetByIdServerFn } from '~/lib/trpc-server';
 
@@ -32,24 +33,26 @@ function RouteComponent() {
   const wishlist = data.data.wishlist;
 
   return (
-    <div className="flex flex-col gap-4 pt-6 pb-4">
-      <div className="flex px-4">
-        <div className="flex flex-col gap-2">
-          <h2 className="text-lg font-medium">{wishlist.title}</h2>
-          <p className="text-sm text-muted-foreground">
-            {wishlist.description}
-          </p>
+    <PageLayout>
+      <div className="flex flex-col gap-4">
+        <div className="flex px-4">
+          <div className="flex flex-col gap-2">
+            <h2 className="text-lg font-medium">{wishlist.title}</h2>
+            <p className="text-sm text-muted-foreground">
+              {wishlist.description}
+            </p>
+          </div>
+          <div className="grow" />
+          <UpdateWishlistDialog wishlist={wishlist}>
+            <AppDialogTrigger variant="outline">
+              <EditIcon />
+            </AppDialogTrigger>
+          </UpdateWishlistDialog>
         </div>
-        <div className="grow" />
-        <UpdateWishlistDialog wishlist={wishlist}>
-          <AppDialogTrigger variant="outline">
-            <EditIcon />
-          </AppDialogTrigger>
-        </UpdateWishlistDialog>
+        <div className="px-2">
+          <Separator />
+        </div>
       </div>
-      <div className="px-2">
-        <Separator />
-      </div>
-    </div>
+    </PageLayout>
   );
 }
